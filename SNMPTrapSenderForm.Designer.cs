dd11becd7c5b5d58afcd183046e697db8433ec50
rahman <rahman.mahmoodi@gmail.com>
2019-07-20 12:06:17 +1000

    init

---
+++ SNMPTrapSenderForm.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace SNMP.Trap.Sender
+{
+    partial class SNMPTrapSenderForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.MessageBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(34, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Send Trap";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // MessageBox
+            // 
+            this.MessageBox.Location = new System.Drawing.Point(34, 64);
+            this.MessageBox.Multiline = true;
+            this.MessageBox.Name = "MessageBox";
+            this.MessageBox.Size = new System.Drawing.Size(478, 337);
+            this.MessageBox.TabIndex = 1;
+            // 
+            // SNMPTrapSenderForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(535, 425);
+            this.Controls.Add(this.MessageBox);
+            this.Controls.Add(this.button1);
+            this.Name = "SNMPTrapSenderForm";
+            this.Text = "SNMP Trap Sender";
+            this.Load += new System.EventHandler(this.SNMPTrapSenderForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox MessageBox;
+    }
+}
+
---
+++ SNMPTrapSenderForm.cs
@@ -0,0 +1,82 @@
+﻿using SnmpSharpNet;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SNMP.Trap.Sender
+{
+    public partial class SNMPTrapSenderForm : Form
+    {
+        private string _Message = "";
+        public SNMPTrapSenderForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SendTrap(MessageBox.Text, GetLocalIPAddress(), 162);
+        }
+
+        private static void SendTrap(string message, string ipAddressToSend, int portToSend)
+        {
+            TrapAgent agent = new TrapAgent();
+
+            // Variable Binding collection to send with the trap
+            VbCollection col = new VbCollection();
+            col.Add(new Oid("1.3.6.1.2.1.1.1.0"), new OctetString(message));
+            //col.Add(new Oid("1.3.6.1.2.1.1.2.0"), new Oid("1.3.6.1.9.1.1.0"));
+            //col.Add(new Oid("1.3.6.1.2.1.1.3.0"), new TimeTicks(2324));
+            col.Add(new Oid("1.3.6.1.2.1.1.4.0"), new OctetString(DateTime.Now.ToString()));
+            col.Add(new Oid("1.3.6.1.2.1.1.1.0"), new OctetString("Critical"));
+
+            // Send the trap to the localhost port 162
+            agent.SendV1Trap(new IpAddress(ipAddressToSend), portToSend, "public",
+                             new Oid("1.3.6.1.2.1.1.1.0"), new IpAddress(GetLocalIPAddress()),
+                             SnmpConstants.LinkUp, 0, 13432, col);
+        }
+
+        public static string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("No network adapters with an IPv4 address in the system!");
+        }
+
+        private void SNMPTrapSenderForm_Load(object sender, EventArgs e)
+        {
+            var message = GetMessage();
+            MessageBox.Text = message;
+        }
+
+        private string GetMessage()
+        {
+            var message =
+                $@"
+Source: {GetLocalIPAddress()} 
+Node: {GetLocalIPAddress()} 
+Type: Event 
+Description: 
+Severity: Critical
+Resource:
+Date and Time: {DateTime.Now.ToString()}
+";
+
+            return message;
+        }
+    }
+}
